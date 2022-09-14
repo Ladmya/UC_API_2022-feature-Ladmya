a610808cbc7441c9c6ea541adb80375271b040d4
Ladmya <ladmya.ioualalen@gmail.com>
2022-09-14 18:40:16 +0200

    Added functions to modify a chef by id in ChefsManager.js: get one/ delete one/ update one & sort all chefs by firstnames or lastnames in ascending order. Also added corresponding controllers in index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
+// Adds chef
 app.post('/chefs', async function(req, res) {
-    const { firstname, lastname, description } = req.body;
-    const chefsManager = new ChefsManager({ firstname, lastname, description });
+    const { firstname, lastname, description, image } = req.body;
+    const chefsManager = new ChefsManager({ firstname, lastname, description, image });
     try {
         const json = await chefsManager.save();
         res.send(json);
@@ -20,9 +21,17 @@ app.post('/chefs', async function(req, res) {
     }
 });
 
+// Gets all chefs
 app.get('/chefs', async function(req, res) {
     const chefsManager = new ChefsManager();
-    try {
+    try {  
+        // Tests ascending sort chefs by firstnames  
+        const testAscendFirstnamesChefs = await chefsManager.getAscendingChefsByFirstnames()
+        console.log(testAscendFirstnamesChefs);
+        // Tests ascending sort chefs by lastnames
+        const testAscendLastnamesChefs = await chefsManager.getAscendingChefsByLastnames()
+        console.log(testAscendLastnamesChefs);
+
         const json = await chefsManager.getChefs()
         res.status(200).json(json);
     } catch (e) {
@@ -31,6 +40,71 @@ app.get('/chefs', async function(req, res) {
     }
 });
 
+//Gets a chef by id
+app.get('/chefs/:id', async function(req,res){
+    const chefsManager = new ChefsManager();
+        try {
+            const json = await chefsManager.getChefById(req.params.id);
+            res.send(json);
+        } catch(e) {
+            console.error('Cannot get chef by Id : error server',e);
+        }
+    });
+
+// Delete a chef
+app.delete('/chefs/:id',async function(req,res){
+    const chefsManager = new ChefsManager();
+    try {
+        const json = await chefsManager.deleteChefById(req.params.id);
+        if(!json) {
+            return res.status(404).send(); 
+        }
+        res.send(json);
+    } catch(e) {
+        console.error('Cannot delete chef : Error server');
+        console.error(e);
+    } 
+});
+
+// Updates a profile : Patch rather than Put
+app.patch('/chefs/:id', async (req, res) => {
+    const chefsManager = new ChefsManager();
+    try{
+        const json = await chefsManager.updateChefById(req.params.id, req.body, { new: true });       
+        return res.status(201).json({json});
+        } catch (e) {
+        console.log('Cannot update profile PATCH : server error',e);
+    }
+});
+
+
+// Je pensais pouvoir tester sur Postman les "sort" via des routes en les créant 
+// mais ça ne fonctionne pas
+
+// Ascending chefs order ???
+
+// app.get('/chefs/lastname', async function(req, res) {
+//     const chefsManager = new ChefsManager();
+//     try {
+//         const json = await chefsManager.getAscendingChefsByLastname()
+//         res.status(200).json(json);
+//     } catch (e) {
+//         console.error('Error server');
+//         console.error(e);
+//     }
+// });
+
+
+// app.put('/chefs/:id', async (req, res) => {
+//     const chefsManager = new ChefsManager();
+//     try{
+//         // const json = await chefsManager.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         const json = await chefsManager.updateChefById(req.params.id, req.body, { new: true });       
+//         return res.status(201).json({json});
+//         } catch (e) {
+//         console.log('Cannot update profile PUT: server error',e);
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`LBA_usecase listening on port ${port}`)
--- managers/ChefsManager.js
+++ managers/ChefsManager.js
@@ -1,28 +1,38 @@
+// ODM : DB structure
+// Model : creates JSON
+
 const Database = require('../database');
 
+// Data template
 const SCHEMA = {
     firstname: String,
     lastname: String,
-    description: String
+    description: String,
+    image: String
 }
-
+// PrivateChefsModel : this.model is the access to Mongoose
 class PrivateChefsModel {
     constructor() {
+        // Creates DB : following SCHEMA template
         const schema = Database.getInstance().mongoose.Schema(SCHEMA);
+        // Creates connexion between Chef collection & schema
         this.model = Database.getInstance().mongoose.model('Chef', schema);
     }
 }
 
 /**
  * Chef model Singleton
+ * Unique instantiation => PrivateChefsModel
  */
 class MongooseModel {
     constructor() {
         throw new Error('Use Database.getInstance()');
+        // Ex: connexion & Auth problems
     }
     static getInstance() {
         if (!MongooseModel.chefsModel) {
             MongooseModel.chefsModel = new PrivateChefsModel();
+            // Creates new object (SCHEMA)
         }
         return MongooseModel.chefsModel;
     }
@@ -31,6 +41,7 @@ class MongooseModel {
 /**
  * Class chef
  * describe chef method with mongoose
+ * way to access the data
  */
 class ChefsManager {
     /**
@@ -39,12 +50,16 @@ class ChefsManager {
      * @param {string} chef.firstname
      * @param {string} chef.lastname
      * @param {string} chef.description
+     * @param {string} chef.image
      */
     constructor(chef = {}) {
         this.model = MongooseModel.getInstance().model;
+        // chef?
         this.chef = new this.model(chef);
     }
 
+    // Mongoose methods in function
+    // Isolated from index.js
     async save() {
         return this.chef.save();
     }
@@ -52,6 +67,28 @@ class ChefsManager {
     async getChefs() {
         return this.model.find();
     }
+    // Get one chef by ID
+    async getChefById(id) {
+        return this.model.findById(id);
+    }
+    // Delete one chef by ID
+    async deleteChefById(id) {
+        return this.model.findByIdAndDelete(id)
+    }
+    // Update one chef by ID
+    async updateChefById(id,body){
+        return this.model.findByIdAndUpdate(id,body);
+    }
+    // Sort lastnames by ascending order
+    // Tested in index.js 
+    async getAscendingChefsByLastnames(){
+        return this.model.find({},{lastname:1}).sort({"lastname":1})
+    }
+    // Sort firstnames by ascending order
+    async getAscendingChefsByFirstnames(){
+        return this.model.find({},{firstname:1}).sort({"firstname":1})
+    }
+
 }
 
 module.exports.ChefsManager = ChefsManager;
